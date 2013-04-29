0a7de7719fddcf4e2b8fa1da742ccc09209e341b
EGartin <elijah.gartin@gmail.com>
2013-04-29 00:16:26 -0600

    Adding all the files to the Repo

---
+++ www/helloScript.js
@@ -0,0 +1,18 @@
+﻿function sayHello(){
+	var nameText = document.getElementById("nameTextField").value;
+	var displayString = "Hello, " + nameText + "!";
+	
+	if(nameText.length <= 0){
+		document.getElementById("nameLabel").innerHTML = "Please enter your name.";
+		document.getElementById("nameTextField").style.background = "red";
+		document.getElementById("nameTextField").style.color = "white";
+		document.getElementById("helloButton").value = "Try Again.";
+	}
+	else{	
+	document.getElementById("nameLabel").innerHTML = displayString;
+	document.getElementById("nameTextField").style.background = "none";
+	document.getElementById("helloButton").value = "Thank you.";
+	}
+	
+	
+}
